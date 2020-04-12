0fdbcb7ab59e3184d74ba9871dafc1ed305ea3a9
Ivan Chin <ivanchin91@hotmail.com>
2020-04-12 16:37:58 +0800

    - Initial commit.

---
+++ DelayedSearchTextBoxDemo/DelayedSearchTextBox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace DelayedSearchTextBoxDemo
+{
+    public class DelayedSearchTextBox : TextBox
+    {
+        private const double _defaultDelay = 300d;
+        private const string DelayedSearchTextBoxName = "TxtBxDelayedSearch";
+
+        private DispatcherTimer _delayTimer;
+
+        public double KeystrokeDelayMilliseconds
+        {
+            get { return (double)GetValue(KeystrokeDelayMillisecondsProperty); }
+            set { SetValue(KeystrokeDelayMillisecondsProperty, value); }
+        }
+        
+        public static readonly DependencyProperty KeystrokeDelayMillisecondsProperty =
+            DependencyProperty.Register(nameof(KeystrokeDelayMilliseconds), typeof(double), typeof(DelayedSearchTextBox), 
+                new PropertyMetadata(_defaultDelay));
+
+        public event RoutedEventHandler OnDelayedSearch;
+
+        static DelayedSearchTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DelayedSearchTextBox),
+                new FrameworkPropertyMetadata(typeof(DelayedSearchTextBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var txtBxDelayedSearch = GetTemplateChild(DelayedSearchTextBoxName) as TextBox;
+
+            txtBxDelayedSearch.TextChanged += TxtBxDelayedSearch_TextChanged;
+
+            // @Ivan
+            // It is important to use a DispatcherTimer instead of a regular Timer so that
+            // objects created by the UI thread is accessible using the Dispatcher queue
+            // provided by the DispatcherTimer.
+            _delayTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(KeystrokeDelayMilliseconds)
+            };
+            _delayTimer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            OnDelayedSearch?.Invoke(sender, null);
+
+            _delayTimer.Stop();
+        }
+
+        private void TxtBxDelayedSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _delayTimer.Stop();
+            _delayTimer.Start();
+        }
+    }
+}
---
+++ DelayedSearchTextBoxDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace DelayedSearchTextBoxDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private Guid _updateID;
+        public Guid UpdateID
+        {
+            get { return _updateID; }
+            set
+            {
+                _updateID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName]string sourcePropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(sourcePropertyName));
+        }
+
+        private void MySearchTextBox_OnDelayedSearch(object sender, RoutedEventArgs e)
+        {
+            // Search logic here.
+
+            // GUID to demonstrate UI updates when the keystroke stops.
+            UpdateID = Guid.NewGuid();
+        }
+    }
+}
